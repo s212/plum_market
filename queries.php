21583681c0121e890a17e3da4a65f811e59bec38
s212 <safaa.tharwat212@gmail.com>
2016-04-22 16:48:57 +0300

    Update queries.php

--- queries.php
+++ queries.php
@@ -1,17 +1,22 @@
 <?php
-include("connection.php")
+include("connection.php");
 class query 
 {
 	private $conn;
-	
+	private $db;
 	public function __construct()
 	{
-		
+    $this->db = Database::getInstance();
+     
+	$this->conn = $this->db->getConnection(); 
+    
+
+		 
 	}
 	public function creat_Account($UserName,$password ,$Email ,$Id)
 	{
-		$sql="INSERT INTO Account (UserName,password ,Email ,Id ) Values ('$username','$password' ,'$email' ,11)";
-            $result = mysqli_query($conn, $sql);
+		$sql="INSERT INTO Account (UserName,password ,Email ,Id ) Values ('$UserName','$password' ,'$Email' ,12)";
+            $result = mysqli_query($this->conn, $sql);
 			if($result)
 					echo "Account created successfully";
 
@@ -20,8 +25,16 @@ class query
 	{
 		
             $sql = "SELECT * FROM Product";
-            $result = mysqli_query($conn, $sql);
+            $result = mysqli_query($this->conn, $sql);
 			return $result;
+	}
+	public function Get_Emails($email)
+	{
+		
+            $sql = "SELECT email FROM Account where Email='".$email."'";
+            $result = mysqli_query($this->conn, $sql);
+			$numResults = mysqli_num_rows($result);
+            return $numResults;
 	}
 	
 	
