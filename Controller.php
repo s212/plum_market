70ea9ae41df5c5261ddc29c5b96b8df33d9002d3
s212 <safaa.tharwat212@gmail.com>
2016-04-22 16:37:58 +0300

    Added files via upload

---
+++ Controller.php
@@ -0,0 +1,97 @@
+<?php
+include("queries.php")
+class query 
+{
+	private $query;
+	
+	public function __construct()
+	{
+		$query=new query();
+	}
+	public function creat_Account()
+	{
+		if(isset($_POST['action']))
+      {          
+        if($_POST['action']=="signup")
+        {
+        $username= $_POST["name"];
+        $email= $_POST["email"];
+        $password=$_POST["password"];
+        $query = "SELECT email FROM Account where Email='".$email."'";
+        $result = mysqli_query($conn,$query);
+        $numResults = mysqli_num_rows($result);
+		
+        if (!filter_var($email, FILTER_VALIDATE_EMAIL)) // Validate email address
+        {
+            echo $message =  "Invalid email address please type a valid email!!";
+        }
+        elseif($numResults>=1)
+        {
+            $message = $email." Email already exist!!";
+        }
+        else
+        {
+	      if($username!=""&&$email!=""&&$password!="")
+		  {
+			
+			$query->creat_Account($username,$password ,$email,1);
+		  }
+	
+         }
+        } 
+     }
+	}
+	public function View_Products($UserName,$password ,$Email ,$Id)
+	{
+		$result = $query->View_Products();
+        $rowcount=mysqli_num_rows($result);
+   if (mysqli_num_rows($result) > 0) 
+    {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result))
+  
+    
+       // echo "NAME: " . $row["Name"]. "Description: " . . " Price,Num" . $row["Price,Num"]. "<br>";
+	   {
+	?>
+	<div class="col-sm-4 col-lg-4 col-md-4">
+                                <div class="thumbnail">
+                                    <!--  <img src="http://placehold.it/320x150" alt="">-->
+                                    <img src="rsz_d.jpg" alt="">
+                                    <div class="caption">
+                                        <h4 class="pull-right"><?php echo "$" .$row["Price"]; ?></h4>
+                                        <h4>
+                                            <a href="#"><?php echo $row["Name"];?></a>
+                                        </h4>
+                                        <p><?php echo $row["Description"];?></p>
+                                    </div>
+                                    <div class="ratings">
+                                        <p class="pull-right">12 reviews</p>
+                                        <p>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                            <span class="glyphicon glyphicon-star"></span>
+                                            <span class="glyphicon glyphicon-star-empty"></span>
+
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+
+<?php						
+      }
+    } 
+
+    else 
+    {
+      echo "0 results";
+    }
+            
+	}
+	
+	
+	
+}
+
+?>
